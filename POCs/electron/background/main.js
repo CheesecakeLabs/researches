75ddc15eceef31fb31b3fbb468e23330f3d16607
Hectore <hectorezoboli@gmail.com>
2021-08-19 18:10:00 -0300

    fix: make icon on dock hide when window is closed

--- POCs/electron/background/main.js
+++ POCs/electron/background/main.js
@@ -1,10 +1,14 @@
 // Modules to control application life and create native browser window
-const {app, Menu, Tray, BrowserWindow} = require('electron')
+const {app, Menu, Tray, nativeImage, BrowserWindow} = require('electron')
 const path = require('path')
+
+let mainWindow;
+
+let systemTray;
 
 function createWindow () {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  const window = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -13,29 +17,59 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  window.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  // window.webContents.openDevTools()
+
+  return window
+}
+
+function createTrayIcon() {
+  //const trayIcon = nativeImage.createFromPath('icon@5x.png')
+  //trayIcon.resize({height: 16, width: 16})
+
+  const tray = new Tray('icon@5x.png')
+
+  const contextMenu = Menu.buildFromTemplate([
+    { 
+      label: 'Show window',
+      type: 'normal',
+      click: (evt, browserWindow) => {
+        if (BrowserWindow.getAllWindows().length) {
+          BrowserWindow.getAllWindows()[0].show()
+        } else {
+          createWindow()
+        }
+
+        if (process.platform === 'darwin') {
+          app.dock.show()
+        }
+      },
+    },
+    { type: 'separator' },
+    {
+      label: 'Quit',
+      type: 'normal',
+      click: (evt, browserWindow) => {
+        app.quit()
+      }
+    }
+  ])
+
+  // Call this again for Linux because we modified the context menu
+  tray.setContextMenu(contextMenu)
+
+  return tray;
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  appIcon = new Tray('icon@5x.png')
-  const contextMenu = Menu.buildFromTemplate([
-    { label: 'Item1', type: 'radio' },
-    { label: 'Item2', type: 'radio' }
-  ])
+  systemTray = createTrayIcon()  
 
-  // Make a change to the context menu
-  contextMenu.items[1].checked = false
-
-  // Call this again for Linux because we modified the context menu
-  appIcon.setContextMenu(contextMenu)
-
-  createWindow()
+  mainWindow = createWindow()
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
@@ -48,7 +82,12 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  //macOS hide dock icon
+  if (process.platform === 'darwin') {
+    app.dock.hide()
+  }
+
+  //if (process.platform !== 'darwin') app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
