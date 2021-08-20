e648ffd68f6f4a44177b868643bd90621bfaf88d
Alexandre Seemund Nicolas <alexandreseemund@gmail.com>
2021-08-20 09:21:35 -0300

    feat: add button to create push notification

--- POCs/electron/push-notifications/renderer.js
+++ POCs/electron/push-notifications/renderer.js
@@ -1,6 +1,10 @@
 const NOTIFICATION_TITLE = 'Title'
-const NOTIFICATION_BODY = 'Notification from the Renderer process. Click to log to console.'
-const CLICK_MESSAGE = 'Notification clicked'
+const NOTIFICATION_BODY = 'Notification from the Renderer process.'
 
-new Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY })
-  .onclick = () => console.log(CLICK_MESSAGE)
+//Button for create push notification
+const btn = document.getElementById("button");
+btn.innerHTML = "Create Push Notification";
+btn.addEventListener("click", function () {
+  new Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY })
+});
+document.body.appendChild(btn);
