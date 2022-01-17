be7781c7b4e4ee3693988d622c06c2b8665c73d3
Diego Bousfield <diegobousfield@gmail.com>
2022-01-17 11:01:38 -0300

    Feat: use browser camera

--- POCs/flutter_web/resources_permissions/lib/main.dart
+++ POCs/flutter_web/resources_permissions/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
+import 'dart:ui' as ui;
 
 void main() {
   runApp(const MyApp());
@@ -7,21 +10,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,84 +25,133 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return const FractionallySizedBox(
+                  heightFactor: 0.5,
+                  widthFactor: 0.5,
+                  child: WebCam(),
+                );
+              });
+        },
+        // tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+class WebCam extends StatefulWidget {
+  const WebCam({Key? key}) : super(key: key);
+
+  @override
+  _WebCamState createState() => _WebCamState();
+}
+
+class _WebCamState extends State<WebCam> {
+  static final html.VideoElement _webcamVideoElement = html.VideoElement();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Register a webcam
+    // ignore: undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory('webcamVideoElement',
+        (int viewId) {
+      getMedia();
+      return _webcamVideoElement;
     });
+  }
+
+  getMedia() {
+    //Change prop "video" to "audio" for using the users mic
+    html.window.navigator.mediaDevices
+        ?.getUserMedia({"video": true}).then((streamHandle) {
+      _webcamVideoElement
+        ..srcObject = streamHandle
+        ..autoplay = true;
+    }).catchError((onError) {
+      print(onError);
+    });
+  }
+
+  switchCameraOff() {
+    if (_webcamVideoElement.srcObject != null) {
+      var tracks = _webcamVideoElement.srcObject?.getTracks();
+
+      //stopping tracks and setting srcObject to null to switch camera off
+      _webcamVideoElement.srcObject = null;
+
+      tracks?.forEach((track) {
+        track.stop();
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    switchCameraOff();
+
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: Stack(
+        children: [
+          HtmlElementView(
+            key: UniqueKey(),
+            viewType: 'webcamVideoElement',
+          ),
+          Column(
+            children: [
+              ElevatedButton(
+                child: const Text('Play/Pause'),
+                onPressed: () async {
+                  if (_webcamVideoElement.paused) {
+                    _webcamVideoElement.play();
+                  } else {
+                    _webcamVideoElement.pause();
+                  }
+                },
+              ),
+              ElevatedButton(
+                child: const Text('Switch off'),
+                onPressed: () {
+                  switchCameraOff();
+                },
+              ),
+              ElevatedButton(
+                child: const Text('Switch on'),
+                onPressed: () {
+                  if (_webcamVideoElement.srcObject == null) getMedia();
+                },
+              ),
+            ],
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
