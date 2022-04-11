7d3a31cc9fb0ddd346db2a6c640724e9f2a68852
Cristian Ferreira <cristianferreira_gks@hotmail.com>
2022-04-11 19:25:31 +0100

    feat: add watch target on app flutter project

---
+++ POCs/watch-flutter-app/watch_flutter/ios/watch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  watch WatchKit Extension
+//
+//  Created by Cristian Santos on 11/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ POCs/watch-flutter-app/watch_flutter/ios/watch WatchKit Extension/RunnerApp.swift
@@ -0,0 +1,19 @@
+//
+//  RunnerApp.swift
+//  watch WatchKit Extension
+//
+//  Created by Cristian Santos on 11/04/22.
+//
+
+import SwiftUI
+
+@main
+struct RunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
